0739f1f5ef3fc6508b8057220bcf10f7080f36d0
Aru <a.demeubaeva@yandex.com>
2020-12-12 06:04:44 +0600

    first commit

---
+++ application/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  application
+//
+//  Created by Aru on 12/12/20.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ application/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  application
+//
+//  Created by Aru on 12/11/20.
+//
+
+import Foundation
